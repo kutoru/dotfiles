f5ec7fd40ab4a545408e63731304bd9258b10f99
kutoru <kutoru.work@gmail.com>
2025-07-04 07:36:34 +0500

    Add harpoon and fugitive & Tweak colorizer

--- home/.config/nvim/lua/plugins/colorizer.lua
+++ home/.config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,32 @@
+local common = {
+  RGB = true,
+  RRGGBB = true,
+  RRGGBBAA = true,
+  names = true,
+
+  rgb_fn = false,
+  hsl_fn = false,
+  css = false,
+  css_fn = false,
+}
+
+local css = {
+  RGB = true,
+  RRGGBB = true,
+  RRGGBBAA = true,
+  names = true,
+
+  rgb_fn = true,
+  hsl_fn = true,
+  css = true,
+  css_fn = true,
+}
+
 return {
   "norcalli/nvim-colorizer.lua",
   opts = {
-    "*",
+    ["*"] = common,
+    css = css,
+    scss = css,
   },
 }
---
+++ home/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,69 @@
+local get_curr_file_path = function()
+  local curr_path = vim.fn.expand("%")
+  local first_char = string.sub(curr_path, 1, 1)
+
+  if first_char == "~" or first_char == "/" or first_char == "." then
+    return curr_path
+  else
+    return "./" .. curr_path
+  end
+end
+
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup({
+      default = {
+        create_list_item = function(_, item)
+          return item
+        end,
+        display = function(item)
+          return item
+        end,
+        equals = function(item_a, item_b)
+          return item_a == item_b
+        end,
+        select = function(item)
+          if item ~= get_curr_file_path() then
+            vim.cmd("e " .. item)
+          end
+        end,
+      },
+    })
+
+    local map = vim.keymap.set
+
+    map("n", "<leader>a", function()
+      harpoon:list():add(get_curr_file_path())
+    end)
+    map("n", "<leader>h", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+    map("n", "<leader>cc", function()
+      harpoon:list():clear()
+    end)
+
+    map("n", "<C-h>", function()
+      harpoon:list():select(1)
+    end)
+    map("n", "<C-j>", function()
+      harpoon:list():select(2)
+    end)
+    map("n", "<C-k>", function()
+      harpoon:list():select(3)
+    end)
+    map("n", "<C-l>", function()
+      harpoon:list():select(4)
+    end)
+
+    map("n", "<leader>p", function()
+      harpoon:list():prev()
+    end)
+    map("n", "<leader>n", function()
+      harpoon:list():next()
+    end)
+  end,
+}
